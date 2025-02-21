7b0bea9aa7e432373527be92c70df8d3a006b728
udud0099 <udud0099@gmail.com>
2025-02-21 09:04:27 +0545

    v19 - Link Component

--- mytry/src/app/layout.js
+++ mytry/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <h1>check layout</h1>
+        <Link href="/products">Product</Link>
+        <Link href="/v9catchAll">v9catchAll</Link>
         {children}
       </body>
     </html>
--- mytry/src/app/v9catchAll/[...slug]/page.jsx
+++ mytry/src/app/v9catchAll/[...slug]/page.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const V9catchAll = async ({ params }) => {
   const { slug } = await params;
-  return <div>V9catchAll {slug}</div>;
+  return <div>
+    
+    V9catchAll {slug}</div>;
 };
 
 export default V9catchAll;
