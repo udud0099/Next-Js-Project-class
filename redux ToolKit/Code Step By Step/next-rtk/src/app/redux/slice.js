7c71ab2fa526fdab466047a24f1fc26d0858cbe6
udud0099 <udud0099@gmail.com>
2025-03-02 19:31:29 +0545

    code step by step - rtk - v6 finish

---
+++ redux ToolKit/Code Step By Step/next-rtk/src/app/redux/slice.js
@@ -0,0 +1,21 @@
+const { createSlice, nanoid } = require("@reduxjs/toolkit");
+
+const initialState = {
+  users: [],
+};
+
+const Slice = createSlice({
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      const data = {
+        id: nanoid(),
+        name: action.name,
+      };
+      state.users.push(data);
+    },
+  },
+});
+
+export const { addUser } = Slice.actions;
+export default Slice.reducer;
