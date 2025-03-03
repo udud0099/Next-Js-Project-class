6ddb1dedc6e810f52b85c72bd29b1ddb2281e2b0
udud0099 <udud0099@gmail.com>
2025-03-03 11:42:05 +0545

    v10 finish

--- redux ToolKit/Code Step By Step/next-rtk2/src/app/components/ListUser.jsx
+++ redux ToolKit/Code Step By Step/next-rtk2/src/app/components/ListUser.jsx
@@ -1,17 +1,26 @@
 "use client";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { removeUser } from "../redux/slice";
 
 const ListUser = () => {
   const userData = useSelector((data) => data.users);
-  console.log(userData);
+  // console.log(userData);
+
+  const dispatch = useDispatch();
 
   return (
     <div>
       <h1>ListUser</h1>
       <div>
-        {userData.map((user) => (
-          <li key={user.id}>{user.name}</li>
+        {userData.map((item) => (
+          <li key={item.id}>
+            <span>
+              {item.name}{" "}
+              <button onClick={() => dispatch(removeUser(item.id))}>D</button>
+            </span>
+          </li>
         ))}
       </div>
     </div>
