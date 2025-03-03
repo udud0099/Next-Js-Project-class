e09dc268f5354e485668da27b484f03215c336f1
udud0099 <udud0099@gmail.com>
2025-03-03 11:25:17 +0545

    v9 finish

--- redux ToolKit/Code Step By Step/next-rtk2/src/app/components/AddUser.jsx
+++ redux ToolKit/Code Step By Step/next-rtk2/src/app/components/AddUser.jsx
@@ -10,12 +10,17 @@ const AddUser = () => {
   const userDispatch = () => {
     // console.log(name);
     dispatch(addUser(name));
+    setName("");
   };
   return (
     <div>
       <h1>AddUser</h1>
       <div>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <button onClick={userDispatch}>add user</button>
       </div>
     </div>
--- redux ToolKit/Code Step By Step/next-rtk2/src/app/components/ListUser.jsx
+++ redux ToolKit/Code Step By Step/next-rtk2/src/app/components/ListUser.jsx
@@ -1,10 +1,19 @@
+"use client";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ListUser = () => {
+  const userData = useSelector((data) => data.users);
+  console.log(userData);
+
   return (
     <div>
       <h1>ListUser</h1>
-      <div></div>
+      <div>
+        {userData.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </div>
     </div>
   );
 };
