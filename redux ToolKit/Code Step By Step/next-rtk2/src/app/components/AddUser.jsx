dc201dfa57e85133a6cbcced164d865d2feaf8c9
udud0099 <udud0099@gmail.com>
2025-03-03 11:05:34 +0545

    v6 finish

---
+++ redux ToolKit/Code Step By Step/next-rtk2/src/app/components/AddUser.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AddUser = () => {
+  return (
+    <div>
+      <h1>AddUser</h1>
+      <div>
+        <input type="text" />
+        <button>add user</button>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ redux ToolKit/Code Step By Step/next-rtk2/src/app/components/ListUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ListUser = () => {
+  return (
+    <div>
+      <h1>ListUser</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default ListUser;
